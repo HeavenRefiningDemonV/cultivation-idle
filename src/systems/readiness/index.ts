7e35e73f19f3a006aeae0e312b8dd2fee3e3b67f
Abdulaziz Hasan <79747913+HeavenRefiningDemonV@users.noreply.github.com>
2026-03-23 08:49:48 +0300

    Add readiness runtime bridge

--- src/systems/readiness/index.ts
+++ src/systems/readiness/index.ts
@@ -2,3 +2,4 @@ export * from './gateBuildFloorTypes.js';
 export * from './gateBuildFloorRegistry.js';
 export * from './readinessScoringTypes.js';
 export * from './readinessScoringEngine.js';
+export * from './readinessRuntime.js';
